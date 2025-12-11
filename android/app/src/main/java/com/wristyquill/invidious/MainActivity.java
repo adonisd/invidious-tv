d415f29f2a94bdf0d27cfa3d526d561bae54ead4
adonisd <adonis.darwish@gmail.com>
2025-12-11 14:21:32 +0100

    init capacitor project

---
+++ android/app/src/main/java/com/wristyquill/invidious/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wristyquill.invidious;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wristyquill.invidious',
+  appName: 'Invidious TV',
+  webDir: 'hosted_web_app'
+};
+
+export default config;
